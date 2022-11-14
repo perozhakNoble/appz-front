af18561fd40d27a4cb7c64be5371ce33b46ceac2
perozhakNoble <vitalii.perozhak@noble.codes>
2022-11-14 22:25:58 +0200

    add all exports & loader

--- src/routes/Expenses.jsx
+++ src/routes/Expenses.jsx
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Outlet } from "react-router-dom";
+import Loader from "../components/Loader";
 import PageHeader from "../components/PageHeader";
 import { getExportFile } from "../services/export";
 
 const categoryOptions = ["Проживання", "Харчування", "Одяг", "Предмети Гігієни", "Дитячі іграшки"];
+const buttonExportClasses = "rounded p-1 px-5 font-bold text-white flex items-center disabled:opacity-70 ";
 
 const Expenses = () => {
-  const downloadPdf = async () => {
-    const url = await getExportFile("pdf");
+  const [isLoading, setLoading] = useState(null);
+
+  const download = async (type = "pdf") => {
+    setLoading(type);
+    const url = await getExportFile(type);
 
     const anchor = document.createElement("a");
     anchor.href = url;
-    anchor.download = "expenses.pdf";
+    anchor.download = "expenses." + type + (type === "xls" ? "x" : "");
 
     document.body.appendChild(anchor);
     anchor.click();
     document.body.removeChild(anchor);
 
     URL.revokeObjectURL(url);
+    setLoading(null);
   };
 
   return (
@@ -40,13 +46,43 @@ const Expenses = () => {
       </Form>
       <div className="mt-16">
         <div className=" flex gap-4 ml-auto w-36 mr-36">
-          <button type="button" className="bg-red-600 rounded p-1 px-5 font-bold text-white" onClick={downloadPdf}>
+          <button
+            type="button"
+            className={`${buttonExportClasses}  bg-red-600 `}
+            disabled={isLoading}
+            onClick={() => download("pdf")}
+          >
+            {isLoading === "pdf" && (
+              <div className="inline-block mr-2">
+                <Loader />
+              </div>
+            )}
             PDF
           </button>
-          <button type="button" className="bg-green-600 rounded p-1 px-5 font-bold text-white">
+          <button
+            type="button"
+            className={`${buttonExportClasses}  bg-green-600 `}
+            disabled={isLoading}
+            onClick={() => download("xls")}
+          >
+            {isLoading === "xls" && (
+              <div className="inline-block mr-2">
+                <Loader />
+              </div>
+            )}
             XLS
           </button>
-          <button type="button" className="border-2 border-cyan-500 rounded p-1 px-5">
+          <button
+            type="button"
+            className={`${buttonExportClasses}  border-cyan-600 border-2`}
+            disabled={isLoading}
+            onClick={() => download("csv")}
+          >
+            {isLoading === "csv" && (
+              <div className="inline-block mr-2">
+                <Loader />
+              </div>
+            )}
             <span className="text-red-600">C</span>
             <span className="text-green-600">S</span>
             <span className="text-indigo-600">V</span>
