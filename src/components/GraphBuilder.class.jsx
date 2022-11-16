6004a9127bd21103ee9a9828ea6842a3606c2eca
perozhakNoble <vitalii.perozhak@noble.codes>
2022-11-17 00:16:37 +0200

    fix pir chart display

--- src/components/GraphBuilder.class.jsx
+++ src/components/GraphBuilder.class.jsx
@@ -113,7 +113,8 @@ export class GraphBuilder {
 
   #renderActiveShape = (props) => {
     const RADIAN = Math.PI / 180;
-    const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value } = props;
+    const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent, value, index } =
+      props;
     const sin = Math.sin(-RADIAN * midAngle);
     const cos = Math.cos(-RADIAN * midAngle);
     const sx = cx + (outerRadius + 10) * cos;
@@ -128,7 +129,7 @@ export class GraphBuilder {
 
     return (
       <g className="z-50">
-        <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+        <text x={cx} y={cy + (index ? 20 : -20)} dy={8} textAnchor="middle" fill={fill}>
           {payload.name}
         </text>
         <Sector
@@ -185,15 +186,7 @@ export class GraphBuilder {
           </BarChart>
         ) : this.#chart === "PieChart" ? (
           <PieChart>
-            <Pie
-              {...this.#pie}
-              data={this.#data}
-              activeIndex={this.#activeIndex}
-              activeShape={this.#renderActiveShape}
-              onMouseEnter={this.#onPieEnter}
-              innerRadius="50%"
-              paddingAngle={2}
-            >
+            <Pie {...this.#pie} data={this.#data} label={this.#renderActiveShape} innerRadius="50%" paddingAngle={2}>
               {this.#data.map((entry, index) => (
                 <Cell key={`cell-${index}`} fill={entry.color} />
               ))}
